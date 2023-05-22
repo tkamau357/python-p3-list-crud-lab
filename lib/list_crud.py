3e5daee8537e6d517d80e0021d1f3583a17be775
johnthairukamau <tkamau82@gmail.com>
2023-05-22 16:44:07 +0300

    Completed assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,82 @@
 def create_an_empty_list():
-    return None
+    return []
+
+create_an_empty_list()
+
 
 def create_a_list():
-    return None
+    my_list = []
+    for i in range(4):
+        element = input("Enter element {} for the list: ".format(i + 1))
+        my_list.append(element)
+    return my_list
 
-def add_element_to_end_of_list(l, element):
-    return None
+my_list = create_a_list()
+print(my_list)
 
-def add_element_to_start_of_list(l, element):
-    return None
 
-def remove_element_from_end_of_list(l):
-    return None
+def add_element_to_end_of_list(my_list, element):
+    my_list.append(element)
+    return my_list
 
-def remove_element_from_start_of_list(l):
-    return None
+new_element = int(input("Enter your last element: "))
+updated_list = add_element_to_end_of_list(my_list, new_element)
+print(updated_list)
 
-def retrieve_first_element_from_list(l):
-    return None
 
-def retrieve_element_from_index(l, index):
-    return None
+def add_element_to_start_of_list(my_list, element):
+    my_list.insert(0, element)
+    return my_list
 
-def retrieve_last_element_from_list(l):
-    return None
+new_element = int(input("Enter your first element: "))
+updated_list = add_element_to_start_of_list(my_list, new_element)
+print(updated_list)
+
+
+def remove_element_from_end_of_list(my_list):
+    if len(my_list) > 0:
+        my_list.pop()
+    return my_list
+
+updated_list = remove_element_from_end_of_list(my_list)
+print(updated_list)
+
+
+def remove_element_from_start_of_list(my_list):
+    if len(my_list) > 0:
+        del my_list[0]
+    return my_list
+
+updated_list = remove_element_from_start_of_list(my_list)
+print(updated_list)
+
+
+def retrieve_first_element_from_list(my_list):
+    if len(my_list) > 0:
+        return my_list[0]
+    else:
+        return None
+
+first_element = retrieve_first_element_from_list(my_list)
+print(first_element)
+
+
+def retrieve_element_from_index(my_list, index):
+    if 0 <= index < len(my_list):
+        return my_list[index]
+    else:
+        return None
+
+index = 2
+element = retrieve_element_from_index(my_list, index)
+print(element)
+
+
+def retrieve_last_element_from_list(my_list):
+    if len(my_list) > 0:
+        return my_list[-1]
+    else:
+        return None
+
+last_element = retrieve_last_element_from_list(my_list)
+print(last_element)
